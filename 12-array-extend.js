cbdd496f45ee959025bc76d5033d4cf577818eaf
Kminseung <soccer6504@gmail.com>
2021-04-06 15:15:46 +0900

    Array map

--- 12-array-extend.js
+++ 12-array-extend.js
@@ -38,4 +38,47 @@ function testFilter() {
     result = source.filter(item => item%2 == 0);    // 짝수만 필터링
     console.log("짝수 필터링(filter):", result);
 }
-testFilter();
+// testFilter();
+
+function testMap() {
+    console.log("-------------- map");
+    const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // source 배열 내부의 모든 요소를 2배
+    // ES5 방식
+    let m = [];
+
+    for(let i=0; i<source.length; i++) {
+        let item = source[i];
+        m.push(item*2);
+    }
+
+    console.log("배열 변형(for):", m);
+
+    // map 함수
+    m = [];
+    m = source.map(item => item * 2);
+    console.log("베열 변형(map):", m);
+}
+
+// testMap();
+
+function testChain() {
+    const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // 원본 배열에서 짝수만 추출해서 2배
+    // ES5 방식
+    let t = [];
+    for(let i=0; i<source.length; i++) {
+        let item = source[i];
+        if(item%2 == 0) {
+            t.push(item * 2);
+        }
+    }
+    console.log("target:", t);
+
+    // filter -> map -> result
+    t = [];
+    t = source.filter(item => item%2 == 0).map(item => item * 2);
+    console.log("target(filter -> map):", t);
+}
+
+testChain();
